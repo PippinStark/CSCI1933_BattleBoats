9a8d47ad4d98d6933d15038d59e0699db2396a4d
PippinStark <31809914+PippinStark@users.noreply.github.com>
2018-02-15 00:00:38 -0600

    Add files via upload

---
+++ Cell.java
@@ -0,0 +1,29 @@
+public class Cell {
+    private int row;
+    private int col;
+    private char status; // ' ': Empty, 'B': Boat, 'H': Hit; 'M': Miss
+
+    public char get_status(){
+        return status;
+    }
+
+    public void set_status(char c){
+        status = c;
+    }
+
+    public Cell(int row, int col, char status){
+        this.row = row;
+        this.col = col;
+        this.status = status;
+    }
+    /*
+    public static void main(String[] args) {
+        Cell a = new Cell(1, 2, 'B');
+        System.out.println(a.get_status());
+        a.set_status('H');
+        System.out.println(a.get_status());
+
+    }
+    */
+
+}
