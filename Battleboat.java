bf6cf32ae890fed3e02bbaae3fe89e012c7c51b8
PippinStark <31809914+PippinStark@users.noreply.github.com>
2018-02-16 13:18:26 -0600

    Update Battleboat.java

--- Battleboat.java
+++ Battleboat.java
@@ -1,32 +1,39 @@
-/*
 public class Battleboat {
-
     private int size;
     private boolean orientation; // false <-> horizontal, true <-> vertical
     private Cell[] spaces;
 
-    // TODO: randomly set the orientation of the boat
-    // TODO: set size of the boat (default to 3-cells long)
     // TODO: declare the Cell objects associated with each boat
 
-    public Battleboat(){
-
+    public Battleboat(boolean orientation, Cell[] spaces, int size){
+        this.orientation = orientation;
+        this.spaces = spaces;
+        if (size != 0) {
+            this.size = size;
+        }
+        else {this.size = 3;}
     }
 
-    // TODO:
     public boolean get_orientation(){
         return orientation;
     }
 
-    // TODO:
     public int get_size(){
-
+        return size;
     }
 
-    // TODO:
+    //ToDo:
     public Cell[] get_spaces(){
-
+        return spaces;
     }
-
-}
+    /*
+    public static void main(String[] args) {
+        Cell[] loc= {new Cell(1,0,'B'), new Cell(1,1,'B'),
+                new Cell(1,3,'B')};
+        Battleboat B = new Battleboat(false, loc, 3);
+        System.out.println(B.get_size());
+        System.out.println(B.get_orientation());
+        System.out.println(B.get_spaces());
+    }
 */
+}
